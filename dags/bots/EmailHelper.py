64c4e7e5bd143a7c7a13494c36ccb9324938284d
dhananatasya <dhanatasya3@gmail.com>
2024-12-04 23:02:34 -0500

    new receiver

--- dags/bots/EmailHelper.py
+++ dags/bots/EmailHelper.py
@@ -2,14 +2,15 @@ import smtplib
 
 def send():
     try:
-        x=smtplib.SMTP('smtp.gmail.com', 587)
+        x = smtplib.SMTP('smtp.gmail.com', 587)
         x.starttls()
         x.login("karimyqueen@gmail.com", "vpiptiqrabukcqur")
-        subject="PROMO BAKMIE SAUDAGAR"
-        body_text="Terdapat Promo Potongan 20% Bakmie Saudagar\nJangan lupa berikan gaya terbaikmu"
-        message="Subject: {}\n\n{}".format(subject,body_text)
+        subject = "PROMO BAKMIE SAUDAGAR"
+        body_text = "Terdapat Promo Potongan 20% Bakmie Saudagar\nJangan lupa berikan gaya terbaikmu"
+        message = "Subject: {}\n\n{}".format(subject, body_text)
         x.sendmail("karimyqueen@gmail.com", "beenwid14@gmail.com", message)
-        print("Succes")
+        x.sendmail("karimyqueen@gmail.com", "dhanatasya3@gmail.com", message)
+        print("Success")
     except Exception as exception:
         print(exception)
         print("Failure")

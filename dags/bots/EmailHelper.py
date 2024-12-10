44d18735a9020c9d3968900e4117f09523109012
Riri <fatihkarimy@gmail.com>
2024-12-10 14:26:58 +0700

    hapus emai dan app pass

--- dags/bots/EmailHelper.py
+++ dags/bots/EmailHelper.py
@@ -4,12 +4,12 @@ def send():
     try:
         x = smtplib.SMTP('smtp.gmail.com', 587)
         x.starttls()
-        x.login("karimyqueen@gmail.com", "vpiptiqrabukcqur")
+        x.login("emailsender", "appspassword")
         subject = "PROMO BAKMIE SAUDAGAR"
         body_text = "Terdapat Promo Potongan 20% Bakmie Saudagar\nJangan lupa berikan gaya terbaikmu"
         message = "Subject: {}\n\n{}".format(subject, body_text)
-        x.sendmail("karimyqueen@gmail.com", "beenwid14@gmail.com", message)
-        x.sendmail("karimyqueen@gmail.com", "dhanatasya3@gmail.com", message)
+        x.sendmail("emailsender", "emailreceiver", message)
+        x.sendmail("emailsender", "emailreceiver", message)
         print("Success")
     except Exception as exception:
         print(exception)
